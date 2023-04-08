4461dbac5d355d3c897480ad5573bbb90a30c46c
trevordiex <trevordiex@gmail.com>
2023-04-08 14:49:04 +0100

    web static task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,11 +14,18 @@ ln -sf -T "$root/releases/test/" "$root/current";
 
 sudo chown -R ubuntu:ubuntu /data
 
-nginxFile=/etc/nginx/sites-available/web;
+nginxFile=/etc/nginx/sites-available;
 
-# remove hbnb_static location if it exitst
-sed -i '/\s*location \/hbnb_static/,/}/d' "$nginxFile";
+for config in web default
+do
+	file="$nginxFile/$config";
+	if [ -e "$file" ]
+	then
+		# remove hbnb_static location if it exitst
+		sed -i '/^\s*location \/hbnb_static/,/}/d' "$nginxFile/$config";
 
-sed -i "/location \/ {/i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" "$nginxFile";
+		sed -i "/^\s*location \/ {/i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" "$nginxFile/$config";
+	fi
+done
 
 nginx -s reload;
