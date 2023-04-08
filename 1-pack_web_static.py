dda1b3e464291671cb5a74ebbf79f2404695465c
trevordiex <trevordiex@gmail.com>
2023-04-08 16:38:54 +0100

    web static task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,9 +2,7 @@
 '''packs web_static folder into an archive'''
 
 import os
-import tarfile
 from datetime import datetime
-from fabric.context_managers import lcd
 from fabric.operations import local
 
 
@@ -12,18 +10,15 @@ def do_pack():
     '''packs a folder into an archive file'''
     d = datetime.now()
     filename = f'web_static_{d.strftime("%Y%m%d%H%M%S")}.tgz'
-    source = './web_static'
 
-    if not os.path.exists('versions/'):
-        os.mkdir('versions')
+    try:
+        local("mkdir -p versions")
 
-    if not os.path.exists('web_static'):
-        return None
+        print(f'Packing web_static to versions/{filename}')
+        run = local(f"tar -cvzf versions/{filename} web_static")
 
-    print(f'Packing web_static to versions/{filename}')
-    run = local(f"tar -cvzf versions/{filename} web_static")
-
-    if run.failed:
-        return None
-    else:
+        if run.failed:
+            return None
         return filename
+    except:
+        return None
