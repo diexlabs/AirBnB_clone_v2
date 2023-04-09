efbb3763230e5ba555464f8e3e894a624651d921
trevordiex <trevordiex@gmail.com>
2023-04-09 02:24:45 +0100

    web static task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from fabric.api import local, run, put, lcd, env
+import os
+
+env.hosts = [
+    '3.94.211.128',
+    '52.91.123.252'
+]
+env.user = 'ubuntu'
+env.key = '/home/trevor/.ssh/school'
+
+
+def do_deploy(archive_path):
+    """ A script that deploys static files to a web server"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        filename = archive_path.rsplit('/', 1)[-1]
+        base = filename.split('.')[0]
+        put(archive_path, f'/tmp/{filename}')
+
+        run("mkdir -p /data/web_static/releases/{}/".format(base))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/".format(filename, base))
+        run("rm -rf /tmp/{}".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(base, base))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(base))
+        return True
+    except Exception as e:
+        return False
+
