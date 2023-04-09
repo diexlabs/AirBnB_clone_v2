f034a5c61f8ab60930c0e681be426699dad83d30
trevordiex <trevordiex@gmail.com>
2023-04-09 02:57:07 +0100

    web static task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from fabric.api import local, run, put, lcd, env
+from fabric.api import run, put, env
 import os
 
 env.hosts = [
@@ -17,15 +17,18 @@ def do_deploy(archive_path):
     try:
         filename = archive_path.rsplit('/', 1)[-1]
         base = filename.split('.')[0]
-        put(archive_path, f'/tmp/{filename}')
 
+        put(archive_path, f'/tmp/{filename}')
         run("mkdir -p /data/web_static/releases/{}/".format(base))
-        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/".format(filename, base))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(filename, base))
         run("rm -rf /tmp/{}".format(filename))
+        run("rsync -a /data/web_static/releases/{}/web_static/*\
+             /data/web_static/releases/{}/".format(base, base))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(base))
         run("rm -rf /data/web_static/current")
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(base, base))
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(base))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(base))
         return True
     except Exception as e:
         return False
-
